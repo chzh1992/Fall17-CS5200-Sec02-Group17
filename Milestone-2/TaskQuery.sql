c43e8bbedd118ac7c27d6c250b8c96bcdcc36f4a
Janhavi Suresh Mahajan <janhavi@ccs.neu.edu>
2017-11-25 20:57:54 -0500

    New file to add the Required task queries

---
+++ Milestone-2/TaskQuery.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user` (`UserId`, `FirstName`, `LastName`, `Email`, `Password`, `Street`, `City`, `Country`, `PostalCode`, `ProfilePic`, `FacultyFlag`, `AdminFlag`)
+VALUES (1001,'User1','User1','user1@gmail.com','user1','Street1','Boston','USA','02115',NULL,0,0);
